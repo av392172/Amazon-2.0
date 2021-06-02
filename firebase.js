1f5c4f680ad92164ee384b9e472829ad22ea9908
Aditya <av392172@gmail.com>
2021-06-02 21:50:30 +0530

    Authentication & Chechout Page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCO_CDP4tUmqQixF9p8GIQuzrgCv-Bw7Vo",
+    authDomain: "amzn-2-307fa.firebaseapp.com",
+    projectId: "amzn-2-307fa",
+    storageBucket: "amzn-2-307fa.appspot.com",
+    messagingSenderId: "508396931756",
+    appId: "1:508396931756:web:55572d0fa9b25028832d09"
+};
